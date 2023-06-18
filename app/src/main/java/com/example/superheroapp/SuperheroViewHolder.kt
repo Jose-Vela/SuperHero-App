0ed2231a85bb2f289054833f8f8facb127dd5edf
Jose-Vela <joseeloyvo@gmail.com>
2023-06-18 02:17:45 -0600

    Añadimos al ImageView de cada item, la imagen correspondiente a cada superheroe de la busqueda:
    - Creamos una nueva data class (SuperheroImageResponse), dentro de SuperHeroDataResponse.kt, para mapear la url de la imagen de cada superheroe (item).
    - Mejoramos la vista de los items (item_superhero.xml), agregando un ImageView que contendrá la imagen de cada superheroe.
    - Creamos un nevo color con transparencia en colors.xml, que será el fondo del nombre de cada superheroe.
    - Agregamos la libreria Picasso el build.gradle (module :app), la cual permite cargar imagenes desde una url.
    - Cargamos la imagen del superheroe a cada item en la función bind de SuperheroViewHolder.kt.

--- app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
+++ app/src/main/java/com/example/superheroapp/SuperheroViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.superheroapp
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.superheroapp.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
 
 class SuperheroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
@@ -10,5 +11,6 @@ class SuperheroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     fun bind(superheroItemResponse: SuperheroItemResponse){
         binding.tvSuperheroName.text = superheroItemResponse.name      // Asignamos el nombre correspondiente al textView de cada item
+        Picasso.get().load(superheroItemResponse.superheroImage.url).into(binding.ivSuperhero)      // Con Picasso cargamos la imagen del superheroe en cada item
     }
 }
