6f3833885fc23d8c131921734f3cf3941fc6a763
Jose-Vela <joseeloyvo@gmail.com>
2023-06-20 17:16:59 -0600

    Se terminó de desarrollar la App:
     - Terminamos de crear la lógica para la vista (pantalla) donde se mostrará la información adicional del superheroe.
     - Se creo la vista donde se muestra la información adicional del superheroe.
     - Se agregaron validaciones para impedir que la aplicación falle cuando no existan resultados relacionados con la busqueda del usuario.
     - Se añadio una icono en la vista principal (ic_search.xml en el directorio drawable), para indicar que no hay resultados para mostrar en pantalla.
     - Se añadio un Toast para dar un mejor Feedback de la busqueda al usuario.

--- app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
+++ app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
@@ -2,12 +2,16 @@ package com.example.superheroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import com.example.superheroapp.databinding.ActivityDetailSuperheroBinding
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperheroActivity : AppCompatActivity() {
 
@@ -21,7 +25,6 @@ class DetailSuperheroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailSuperheroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_detail_superhero)
         val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
         getSuperheroInformation(id)
     }
@@ -31,13 +34,36 @@ class DetailSuperheroActivity : AppCompatActivity() {
             val superheroDetail = getRetrofit().create(ApiService::class.java).getSuperheroDetail(id)
 
             if (superheroDetail.body() != null){
-                runOnUiThread { createUI(superheroDetail.body()!!) }
+                runOnUiThread { createUI(superheroDetail.body()!!) }    // Los dobles signos de admiración !!, indican que puede ser null, aunque aseguramos que no lo será por la validación del if pero debemos colocarlo para evitar que nos muestre que hay un error
             }
-
         }
     }
 
     private fun createUI(superhero: SuperHeroDetailResponse) {
+        Picasso.get().load(superhero.image.url).into(binding.ivSuperhero)
+        binding.tvSuperheroName.text = superhero.name
+        prepareStats(superhero.powerstats)
+        binding.tvSuperheroRealName.text = superhero.biography.fullName
+        binding.tvPublisher.text = superhero.biography.publisher
+    }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewPower, powerstats.power)
+        updateHeight(binding.viewCombat, powerstats.combat)
+    }
+
+    private fun updateHeight(view: View, stat: String){
+        val params = view.layoutParams
+        if(stat != "null") params.height = pxToDp(stat.toFloat()) else params.height = pxToDp(0.00.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
     }
 
     private fun getRetrofit(): Retrofit {
--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var retrofit: Retrofit
 
     private lateinit var adapter: SuperheroAdapter
+
+    private lateinit var toast: Toast
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     private fun initUI() {
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.i("josé", "Presionado :$query")
                 searchByName(query.orEmpty())   // Si query es null, devolverá un texto vacio que se enviará a searchByName()
                 return false
             }
@@ -40,7 +44,9 @@ class MainActivity : AppCompatActivity() {
             override fun onQueryTextChange(newText: String?) = false
         })
 
-        adapter = SuperheroAdapter{ superheroId -> navigateToDetail(superheroId) }
+        binding.ivEmptyResults.isVisible = true
+
+        adapter = SuperheroAdapter { superheroId -> navigateToDetail(superheroId) }
         binding.rvSuperhero.setHasFixedSize(true)
         binding.rvSuperhero.layoutManager = LinearLayoutManager(this)
         binding.rvSuperhero.adapter = adapter
@@ -49,21 +55,29 @@ class MainActivity : AppCompatActivity() {
     private fun searchByName(query: String) {
         binding.progressBar.isVisible = true
         CoroutineScope(Dispatchers.IO).launch { // Lanza una corrutina. Dispatchers.IO indica que lo lance en un hilo secundario
-            val myResponse: Response<SuperHeroDataResponse> = retrofit.create(ApiService::class.java).getSuperheroes(query)
-            if(myResponse.isSuccessful){
-                Log.i("josé","Funciona :)")
-                val response: SuperHeroDataResponse? = myResponse.body()
-                if(response != null){
-                    Log.i("josé",response.toString())
-                    runOnUiThread {     // Correr en el 'hilo principal' lo que esté entre las llaves. Solo el 'hilo principal' puede crear vistas
-                        adapter.updateList(response.superheroes)
-                        binding.progressBar.isVisible = false
-                    }
+            val myResponse: Response<SuperHeroDataResponse> =
+                retrofit.create(ApiService::class.java).getSuperheroes(query)
+            if (myResponse.isSuccessful) {
+                val response: SuperHeroDataResponse = myResponse.body()!!
+                if (response.response != "error") {
+                    runOnUiThread { showResults(response.superheroes, false) }     // Correr en el 'hilo principal' lo que esté entre las llaves. Solo el 'hilo principal' puede crear vistas
+                } else {
+                    runOnUiThread { showResults(emptyList(), true) }
                 }
-            } else{
-                Log.i("josé","No funciona :(")
+            } else {
+                runOnUiThread { showResults(emptyList(), true) }
             }
         }
+    }
+
+    private fun showResults(result: List<SuperheroItemResponse>, emptyResults: Boolean ) {
+        var textToast = "Sin Resultados"
+        adapter.updateList(result)
+        binding.progressBar.isVisible = false
+        binding.ivEmptyResults.isVisible = emptyResults
+        if (!emptyResults) textToast = "Mostrando Resultados"
+        toast = Toast.makeText(this, textToast, Toast.LENGTH_LONG)
+        toast.show()
     }
 
     private fun getRetrofit(): Retrofit {
@@ -74,7 +88,7 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    private fun navigateToDetail(id: String){
+    private fun navigateToDetail(id: String) {
         val intent = Intent(this, DetailSuperheroActivity::class.java)
         intent.putExtra(EXTRA_ID, id)
         startActivity(intent)
